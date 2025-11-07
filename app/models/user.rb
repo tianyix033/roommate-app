e7e44cbba9fb6569e0c5489a9401cc14bd1e6ab6
MoayadAlismail <moayadalismail@gmail.com>
2025-11-06 23:51:32 -0500

    feat: validate user email presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
   has_many :listings
 
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :email, presence: true
 end
