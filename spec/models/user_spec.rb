cea43805da50782046132c0e1e49cb7f7f5acebe
MoayadAlismail <moayadalismail@gmail.com>
2025-11-06 22:57:57 -0500

    Add failing model spec for user email validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    it 'is invalid without an email' do
+      user = User.new(password: 'secret123')
+
+      expect(user).not_to be_valid
+      user.validate
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+  end
 end
