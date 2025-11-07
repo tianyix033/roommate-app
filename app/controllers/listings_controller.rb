1715ae9467aeb09719f8d3976db13f46bf968910
Steven Li <lishuyustevenli@163.com>
2025-11-07 15:15:43 -0500

    Make GET /search/listings request spec pass

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,5 +1,12 @@
 class ListingsController < ApplicationController
   def search
-    head :not_implemented
+    filters = params.slice(:city, :min_price, :max_price, :keywords).to_unsafe_h.symbolize_keys
+    @listings = Listing.search(filters)
+
+    if @listings.any?
+      render plain: @listings.pluck(:title).join(', ')
+    else
+      render plain: 'No results found'
+    end
   end
 end
