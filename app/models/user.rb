8839a29e874a0628e116aa7435e6ce1efc7f3c9c
MoayadAlismail <moayadalismail@gmail.com>
2025-11-06 22:59:58 -0500

    Make user email uniqueness spec pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :listings
 
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true
 end
