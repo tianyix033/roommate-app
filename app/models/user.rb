b20273c738ed3d12f9298f01680fe1e47207db24
mAE7777 <sitaoma7@gmail.com>
2025-11-19 02:53:19 -0500

    Make user role default spec pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ApplicationRecord
   has_many :listings, dependent: :destroy
   has_one :avatar, dependent: :destroy
 
+  ROLES = %w[admin member].freeze
+
+  before_validation :set_default_role
+  validates :role, inclusion: { in: ROLES }
+
   validates :email, presence: true
   validates :display_name, presence: true, if: :profile_display_name_required?
   validates :budget,
@@ -14,5 +19,9 @@ class User < ApplicationRecord
 
   def profile_display_name_required?
     display_name.present? || will_save_change_to_display_name?
+  end
+
+  def set_default_role
+    self.role ||= 'member'
   end
 end
